5bf6fd02a46b0075e39b563de8b229a71e40dae7
Dan Robertson <dan@productintuition.com>
2018-06-21 16:51:01 -0700

    Fetch 12 random users via API

--- js/page.js
+++ js/page.js
@@ -1,1 +1,21 @@
-// To-do
+
+function checkStatus(response) {
+	if(response.ok) {
+		return Promise.resolve(response);
+	} else {
+		return Promise.reject(new Error(response.statusText));
+	}
+}
+
+function fetchData(url) {
+	return fetch(url, {mode: 'cors'})
+		.then(checkStatus)
+		.then(res => res.json())
+		.catch(error => console.log('Looks like there was a problem', error))
+}
+console.log('this worked so far... [line 16]');
+
+fetchData('https://randomuser.me/api/?results=12')
+	.then(data => console.log(data));
+
+
