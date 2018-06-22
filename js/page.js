48429ba2669e6282fb1ff786d5946607e4768015
Dan Robertson <dan@productintuition.com>
2018-06-22 13:28:17 -0700

    Add CSS and placeholder HTML for Details modal

--- js/page.js
+++ js/page.js
@@ -19,3 +19,28 @@ fetchData('https://randomuser.me/api/?results=12&nat=US')
 	.then(data => console.log(data));
 
 
+// Get the modal
+var modal = document.getElementById('details-modal');
+
+// Get the button that opens the modal
+var contact = document.querySelector('.contact');
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the contact, open the modal 
+contact.onclick = function() {
+    modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+    modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
